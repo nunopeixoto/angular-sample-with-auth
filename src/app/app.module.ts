468d1f1efeec86919893d1451809b727f725fc65
Peixoto <nuno_peixoto1997@hotmail.com>
2022-04-05 18:37:01 +0100

    Create sample sign up component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSliderModule } from '@angular/material/slider';
-
+import { AuthModule } from './auth/auth.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -15,7 +14,8 @@ import { MatSliderModule } from '@angular/material/slider';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule
+    SharedModule,
+    AuthModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
