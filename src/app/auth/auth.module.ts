2b06625534f1cdf8efd9e26eeb071728b94afd57
Peixoto <nuno_peixoto1997@hotmail.com>
2022-04-05 19:08:06 +0100

    Add router and routing between login and signup

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,17 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { SharedModule } from '../shared/shared.module';
+import { LoginComponent } from './login/login.component';
+import { AuthRoutingModule } from './auth-routing.module';
 
 @NgModule({
   declarations: [
-    SignUpComponent
+    SignUpComponent,
+    LoginComponent
   ],
   imports: [
+    AuthRoutingModule,
     CommonModule,
     SharedModule
   ],
   exports: [
-    SignUpComponent
+    SignUpComponent,
+    LoginComponent
   ]
 })
 export class AuthModule { }
---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html'
+})
+export class LoginComponent implements OnInit {
+  hide = true;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
