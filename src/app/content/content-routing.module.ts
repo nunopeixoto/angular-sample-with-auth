76c944ef03ec1272303b9706f9a717a66936ed17
Peixoto <nuno_peixoto1997@hotmail.com>
2022-04-05 23:59:35 +0100

    Add auth guard

--- src/app/content/content-routing.module.ts
+++ src/app/content/content-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '../shared/auth.guard';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: HomeComponent
+    component: HomeComponent,
+    canActivate: [AuthGuard]
   },
 ];
 
