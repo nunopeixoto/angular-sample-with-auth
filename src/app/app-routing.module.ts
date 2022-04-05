037957d3e2f25e7141d3c806b6648a13048e50f9
Peixoto <nuno_peixoto1997@hotmail.com>
2022-04-05 19:18:04 +0100

    Setup basic 404 component and login redirect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {
     path: 'auth',
     loadChildren: () => import('src/app/auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: '',   redirectTo: '/auth/login', pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
   }
 ];
 
